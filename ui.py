606a735bdd77d461c55c3f64d1343223d61ca9f3
Rael0515 <perey0515@naver.com>
2026-03-10 19:40:25 +0900

    일반 모드 추가

--- ui.py
+++ ui.py
@@ -10,12 +10,14 @@ def menu() :
         "|"+" Try selecting options directly to make " + "|\n" +
         "|"+" the prompt more precise.               " + "|\n\n" 
     )
-     #"|"+" Select Menu with number 1,2,3          " + "|\n" +
+    #"|"+" Select Menu with number 1,2,3          " + "|\n" +
         #"  > 1.Add options to existing prompts.    " + "|\n" +
         #"  > 2.Build prompts with Tag.             " + "|\n" +
         #"  > 3.Exit the program.                   " + "|\n" 
+    print("프롬프트를 사용하는 목적을 입력해 주세요(ex : flower)")
     
 def intro_option():
-        print("Available options : ")
-        for key in OPTIONS:
-            print(" - ", key)
+    # 이미지 프롬프트에 사용할 수 있는 태그(옵션)를 표시
+    print("Available options : ")
+    for key in OPTIONS:
+        print(" - ", key)
